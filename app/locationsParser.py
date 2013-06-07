043c1ca465669ffb36e9b681cea0f4c5a6d583d5
Rahul Bhattacharya <r3bhatta@gmail.com>
2013-06-07 18:00:33 -0400

    modified location parsing algorithm to remove duplicate strings within same string too

--- app/locationsParser.py
+++ app/locationsParser.py
@@ -9,6 +9,11 @@ import requests
 
 addresses = []
 
+def unique_list(l):
+    ulist = []
+    [ulist.append(x) for x in l if x not in ulist]
+    return ulist
+
 def locationCheck(tag):
     for key in dict(tag.attrs):
         if key.find('location') != -1:
@@ -38,16 +43,17 @@ def parseSingleSoup(soup):
                     allText = locationSoup.find_all(text = True)
                     index = 0
                     for text in allText:
-                        
-                        'Remove all extra white spaces, make them into a single whitespace'
-                        cleanText = re.sub(r'\s+', ' ', text)  
                         'Remove script tags if they got through'
                         removeScriptTagsPattern = re.compile(r"[{}\[\]]")
-                        
-                        if re.search(removeScriptTagsPattern, cleanText) is None:
+
+                        if re.search(removeScriptTagsPattern, text) is None:
+                            cleanText = text
+                            
                             address = None
                             try:
                                 parsedAdress = addressParser.parse_address(cleanText).full_address()
+                                'If the address parser did not break this code, it means that it passed the parsing'
+                                'Retain the old string, as the address parser parses it funny, but we use its validation'
                                 index += 1
                                 siblingIndex = index + 1
                                 maxSiblingSearchDepth = 4
@@ -57,7 +63,7 @@ def parseSingleSoup(soup):
                                     siblingText = str(allText[siblingIndex])
 
                                     if re.sub(r'\s+', '', siblingText) != re.sub(r'\s+', '', cleanText):
-                                        cleanText += re.sub(r'\s+', ' ', siblingText)   
+                                        cleanText += " " + siblingText
                                     # Found 3 > numbers in a row, hopefully it's a postal code.
                                     if len(re.findall(r"\D(\d[0-9]{3,})\D", ' ' + siblingText + ' ')) > 0:
                                         break;
@@ -66,12 +72,14 @@ def parseSingleSoup(soup):
                                     siblingSearchDepth += 1
 
                                 if cleanText not in addresses:
-                                    #if len(addresses) == 0:
+
+                                    'Remove duplicate and make all white spaces look nice'
+                                    cleanText = ' '.join(unique_list(cleanText.split(" ")))
+                                    cleanText = re.sub(r'\s+', ' ', cleanText)
                                     addresses.append(cleanText)
+                                    
                             except:
                                 pass
-                        
-
 def parseLocations(soups):
     for soup in soups:
         parseSingleSoup(soup)
