d46aa3f5ab1b32c10306fb9f186988cb4b13f5a2
Rahul Bhattacharya <r3bhatta@gmail.com>
2013-06-11 17:16:59 -0400

    trying to imrpve parser algorithm, note main.py is pointin to the trial location file

--- app/Main.py
+++ app/Main.py
@@ -2,7 +2,7 @@ import threading
 import sys
 
 import jsonParser
-import locationsParser
+import locationParserTrial as locationsParser
 
 # Set default encoding to UTF to avoid conflicts with symbols.
 reload(sys)
@@ -13,10 +13,15 @@ soups = jsonParser.parse()
 ##### LOCATIONS ###################################################################
 def locationCBFunc(formattedAddresses):
     print 'Formatted Addresses in thread: %s' % threading.current_thread().name
+    '''
     for address in formattedAddresses:
         print address
+        print(address.encode('cp1252'))
+    '''
 
 def locationsThread(callback):
     callback(locationsParser.parseLocations(soups))
 
 thr = threading.Thread(target=locationsThread, name="locationThread", args=(locationCBFunc,)).start()
+
+
