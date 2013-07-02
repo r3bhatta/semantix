060eebf01a3c71f2477e1ac52dc9f2526b2716d8
Rahul Bhattacharya <r3bhatta@gmail.com>
2013-07-02 17:53:37 -0400

    made locations a little cleaner, filtered  out bad results

--- app/Main.py
+++ app/Main.py
@@ -1,26 +1,30 @@
 import threading
 import sys
-
+import os
 import jsonParser
-import locationParserTrial as locationsParser
+import locationsParser 
 
 # Set default encoding to UTF to avoid conflicts with symbols.
 reload(sys)
 sys.setdefaultencoding('utf-8')
+WINDOWS = 'nt'
 
 soups = jsonParser.parse()
 
-##### LOCATIONS ###################################################################
-def locationCBFunc(formattedAddresses):
+##### LOCATIONS ###################################################################################
+def locationCallBack(allSoupsAddresses):
     print 'Formatted Addresses in thread: %s' % threading.current_thread().name
     
-    for address in formattedAddresses:
-        print address
-        print(address.encode('cp1252'))
-
+    for singleSoupAddresses in allSoupsAddresses:
+        for address in singleSoupAddresses:
+            
+            if os.name == WINDOWS:
+                print(address.encode('cp1252'))            
+            else:
+                print address
+    
 def locationsThread(callback):
     callback(locationsParser.parseLocations(soups))
 
-thr = threading.Thread(target=locationsThread, name="locationThread", args=(locationCBFunc,)).start()
-
+thr = threading.Thread(target=locationsThread, name="locationThread", args=(locationCallBack,)).start()
 
--- app/locationsParser.py
+++ app/locationsParser.py
@@ -1,112 +1,40 @@
 from bs4 import BeautifulSoup
-from address import AddressParser, Address
-import jsonParser
-import stringSimilarity
 import re
-import quopri
-import json
-import os, errno
-import requests
 
-addresses = []
-STRING_MATCH_TOLERANCE = 0.4
-SIBLING_SEARCH_DEPTH = 4
 
-def unique_list(l):
-    ulist = []
-    [ulist.append(x) for x in l if x not in ulist]
-    return ulist
+commonlyAppearingIncorrectParses = set(["google","analytics","tag","ads"])
 
-def locationCheck(tag):
-    for key in dict(tag.attrs):
-        if key.find('location') != -1:
-            return True
-        if type(tag[key]) is unicode:
-            if str(tag[key]).find('location') != -1:
-                return True
-        else:
-            if any('location' in s for s in tag[key]):
-                return True
-    return False
 
 def parseSingleSoup(soup):
-    title = soup.title
+    
+    removeScriptTagsPattern = re.compile(r"[{}\[\]\*>=]")
+    finalAddresses = []
+    if soup is not None:
+        pageTitle = soup.title
+        finalText = ""
+        if pageTitle is not None and 'location' in str(pageTitle).lower():
 
-    if title is not None and 'location' in str(title):
-        r = re.compile(r'location', re.IGNORECASE)          
-        locationTags = soup.find_all(locationCheck)
+            ''' TODO: what if the location is not in the page title?  '''
+            ''' TODO: now this needs to call the naive bayes method to give % probablities of topic and if is subtopic of location print it'''
 
-        if locationTags:
-            addressParser = AddressParser()
-              
-            for locationTag in locationTags:
-                if str(locationTag.name) != 'script':
-                    locationSoup = BeautifulSoup(str(locationTag))
-                    # Extract the text value from all the tags.
-                    allText = locationSoup.find_all(text = True)
-                    index = 0
-                    for text in allText:
-                        'Remove script tags if they got through'
-                        removeScriptTagsPattern = re.compile(r"[{}\[\]]")
-
-                        if re.search(removeScriptTagsPattern, text) is None:
-                            cleanText = text
-                            
-                            address = None
-                            try:
-                                parsedAdress = addressParser.parse_address(cleanText).full_address()
-                                'If the address parser did not break this code, it means that it passed the parsing'
-                                'Retain the old string, as the address parser parses it funny, but we use its validation'
-                                index += 1
-                                siblingIndex = index + 1
-                                siblingSearchDepth = 0
-
-                                while (siblingSearchDepth < SIBLING_SEARCH_DEPTH):
-                                    siblingText = str(allText[siblingIndex])
-
-                                    if re.sub(r'\s+', '', siblingText) != re.sub(r'\s+', '', cleanText):
-                                        cleanText += " " + siblingText
-                                    # Found 3 > numbers in a row, hopefully it's a postal code.
-                                    if len(re.findall(r"\D(\d[0-9]{3,})\D", ' ' + siblingText + ' ')) > 0:
-                                        break;
-
-                                    siblingIndex += 1
-                                    siblingSearchDepth += 1
-
-                                if cleanText not in addresses:
-
-                                    'Remove duplicates within same string and make all white spaces look nice'
-                                    cleanText = ' '.join(unique_list(cleanText.split(" ")))
-                                    cleanText = re.sub(r'\s+', ' ', cleanText)
-                                    addresses.append(cleanText)
-                                    
-                            except:
-                                pass
-
-def filterDuplicateAddresses():
-    filteredAddresses = []
-    for address1 in addresses[:]:
-        
-        if address1 not in addresses:
-            continue
-
-        addresses.remove(address1)
-        for address2 in addresses[:]:
-            similarity = stringSimilarity.compute(address1, address2)
-            if similarity > STRING_MATCH_TOLERANCE:
-                addresses.remove(address2)
-
-                if len(address2) > len(address1):
-                    address1 = address2
-
-        filteredAddresses.append(address1)
-    print "\n" + str(len(filteredAddresses)) + " addresses found \n"
-    return filteredAddresses
+            divTagsCollection = soup.findAll('div')
+            for divTag in divTagsCollection:
+                divTagTextCollection = divTag.findAll(text=True, recursive=False)
+                finalText = ""
+                for text in divTagTextCollection:        
+                    if len(re.sub(r'\s+', '', text)) > 0:
+                        if re.search(removeScriptTagsPattern, text) is None and not any(word in commonlyAppearingIncorrectParses for word in text.lower().split()):
+                            finalText += str(re.sub(r'\s+', ' ', text))
+            
+                if finalText:
+                    finalAddresses.append(finalText)             
+    return finalAddresses     
 
 def parseLocations(soups):
+    finalAddressesAllSoups = []
     for soup in soups:
-        parseSingleSoup(soup)
+        finalAddress = parseSingleSoup(soup)
+        if finalAddress is not None and len(finalAddress) > 0:
+            finalAddressesAllSoups.append(finalAddress)
 
-    for address in addresses:
-        print address
-    return filterDuplicateAddresses()
+    return finalAddressesAllSoups
