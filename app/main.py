14ab07895ad6fe8b07c0cb64b6ce76a0af89b65e
Rahul Bhattacharya <r3bhatta@gmail.com>
2013-07-13 11:56:14 -0400

    fixed file structure, runs from ./start and main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
+import os
 import sys
+sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
+import settings
 from parsers import jsonparser as JsonParser
 from parsers import contextparser as ContextParser
+from naivebayesclassifier.naivebayesclassifier import NaiveBayesClassifier
+
 WINDOWS = 'nt'
-
-# Set default encoding to UTF to avoid conflicts with symbols.
 reload(sys)
-sys.setdefaultencoding('utf-8')
+sys.setdefaultencoding('utf-8') # Set default encoding to UTF to avoid conflicts with symbols.
 
-soups = JsonParser.parseData()
-contextMap = ContextParser.parseSoups(soups)
+
+soups = JsonParser.parseData(settings.CPK_DATA)
+nbc = NaiveBayesClassifier(settings.APP_DATA_TRAINING)
+contextMap = ContextParser.parseSoups( soups, nbc )
 
 #  NOTE : contextMap may have repeats of similar texts, it needs to run through string comparison taking bests
 
 # print contextMap
+
