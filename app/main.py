066fe5342a2943f2201752867fa023bc3da31a87
Rahul Bhattacharya <r3bhatta@gmail.com>
2013-07-17 17:52:16 -0400

    pre pull commit

--- app/main.py
+++ app/main.py
@@ -22,11 +22,11 @@ WINDOWS = 'nt'
 reload(sys)
 sys.setdefaultencoding('utf-8') # Set default encoding to UTF to avoid conflicts with symbols.
 
-'''
-soups = JsonParser.parseData(settings.CPK_DATA)
-nbc = NaiveBayesClassifier(os.path.join(settings.APP_DATA_TRAINING, 'general'))
-contextMap = ContextParser.parseSoups(soups, nbc)
-'''
+
+#soups = JsonParser.parseData(settings.CPK_DATA)
+#nbc = NaiveBayesClassifier(os.path.join(settings.APP_DATA_TRAINING, 'general'))
+#contextMap = ContextParser.parseSoups(soups, nbc)
+
 
 # NOTE: contextMap may have repeats of similar texts, it needs to run through string comparison
 # taking bests.
