3c53dbc2462076fa76888621cf08dd49990050b7
Raymond Huang <huangrayr@gmail.com>
2013-07-27 20:48:09 -0400

    Modified to label:sublabel.

--- app/naivebayesclassifier/naivebayesclassifier.py
+++ app/naivebayesclassifier/naivebayesclassifier.py
@@ -8,84 +8,75 @@ from os.path import isfile, join
 Example:
 nbc = NaiveBayesClassifier('/path/to/training/folder')
 item = nbc.classify('classify this')
-# If you want to train other preset data, do this:
-nbc.train('/path/to/training/folder')
 """
 class NaiveBayesClassifier:   
     """
-    These private variables are initialized by self.train()
-        self.labels - ['label1, label2']
-        self._trainingSet - {'feature': ['label1, label2']}
-        self._featuresSet - {'feature: {'label1': 1, 'label2': 0}}
-        self._labelProbabilityDistribution
-        self._featureProbabilityDistribution
+    These private variables are initialized by self._train()
+        self._labels - ['label1, label2']
+        self._trainingset - {'feature': ['label1, label2']}
+        self._featuresset - {'feature: {'label1': 1, 'label2': 0}}
+        self._featureprobabilitydistribution
 
-    @param trainingDirectory A valid absolute path from the training directory.
+    @param trainingfolders An array of valid absolute paths.
+    @param commonwordsfolder Aa valid absolute path to the common words folder.
     """
-    def __init__(self, trainingDirectory, commonWordsDirectory):
-        if trainingDirectory is None:
+    def __init__(self, trainingfolders, commonwordsfolder):
+        if trainingfolders is None:
             raise Exception('Please input an absolute path training directory.')
 
         # Start training.
-        self._commonWords = self._getCommonWords(commonWordsDirectory)
-        self._trainingDirectory = trainingDirectory
-        self.train(self._trainingDirectory)
+        self._commonwords = self._getCommonWords(commonwordsfolder)
+        self._trainingfolders = trainingfolders
+        self._trainingset = defaultdict(list)
+        self._labels = set()
+        self._generateTrainingSet()
+        self._generateFeaturesSet()
+        self._generateFeatureProbabilityDistribution()
 
-    """ Creates and returns a training set (dictionary) from one data file belonging to a label. """
-    def _updateTrainingSet(self, fileName, label):
-        trainingSet = defaultdict(list)
-        with open(fileName) as trainingFile:
-            lines = trainingFile.readlines()
+    """
+    Creates and returns a training set (dictionary) from one data file belonging to a label.
+    """
+    def _updateTrainingSet(self, filename, label):
+        trainingset = defaultdict(list)
+        with open(filename) as trainingfile:
+            lines = trainingfile.readlines()
             for line in lines:
-                trainingSet[line.strip()].append(label)
+                trainingset[line.strip()].append(label)
         # Add the new features:labels to the training set.
-        for item, labels in trainingSet.items():
-            self._trainingSet[item].extend(labels)
+        for item, labels in trainingset.items():
+            self._trainingset[item].extend(labels)
 
     """
     Ensures that common words are not considered to avoid under representing matched words.
     @returns A list of common words extracted from the common words directory.
     """
-    def _getCommonWords(self, commonWordsDirectory):
-        commonWords = [] 
-        for root, dirs, files in os.walk(commonWordsDirectory):
+    def _getCommonWords(self, commonwordsfolder):
+        commonwords = [] 
+        for root, dirs, files in os.walk(commonwordsfolder):
             for file in files:
                 if file.endswith(".txt"):
-                    f = open(os.path.join(commonWordsDirectory,file), 'r')
+                    f = open(os.path.join(commonwordsfolder, file), 'r')
                     for line in f:
-                        commonWords.append(line.strip('\r\n') )
+                        commonwords.append(line.strip('\r\n') )
                     f.close()
-        return commonWords
+        return commonwords
 
     """
     Train the classifier by iterating through the folder that contains the data.
-    @param trainingDataType = 'data' or 'businesses' so far.
     """
     def _generateTrainingSet(self):
-        # Use a defaultdict(list) because the same feature can belong to multiple labels.
-        self._trainingSet = defaultdict(list)
-        self.labels = []
-        # Ignore some OS generated files, as well as default folders that should not be included.
-        # For example, we ignore 'businesses', but if we need 'businesses' we will only be looking
-        # inside 'businesses' folder and nothing else.
-        # Could be changed!
-        ignores = ['.DS_Store']
-        trainingDirectory = self._trainingDirectory
-
-        # Loop through each folder name for the training folder. 
-        # The folder name corresponds to a label.
-        for label in listdir(trainingDirectory):
-            if label not in ignores:
-                # Add the folder name as a 'label'.
-                self.labels.append(label)
-                # Obtain the absolute path of the folder.
-                path = os.path.join(trainingDirectory, label)
-                # Loop through each training file of each folder.
-                for fileName in [f for f in listdir(path) if isfile(join(path, f))]:
-                    # Obtain the absolute path of the file.
-                    absFileName = os.path.join(path, fileName)
-                    # Update the training set dictionary with the training set from the file.
-                    self._updateTrainingSet(absFileName, label)
+        ignores = [".DS_Store"]
+        for name, folderpath in self._trainingfolders.items():
+            if folderpath not in ignores:
+                for sublabel in listdir(folderpath):
+                    if sublabel not in ignores:
+                        label = str(name) + ":" + str(sublabel)
+                        self._labels.add(label)
+                        sublabelpath = os.path.join(folderpath, sublabel)
+                        with open(sublabelpath) as trainingfile:
+                            lines = trainingfile.readlines()
+                            for line in lines:
+                                self._trainingset[line.strip()].append(label)
 
     """
     Tokenize the input, perform some label specific feature work, and assign to kvp with
@@ -101,7 +92,7 @@ class NaiveBayesClassifier:
         numOrdinals = 1
 
         for word in words:
-            if word not in self._commonWords:
+            if word not in self._commonwords:
                 """
                 LOCATION FEATURES SPECIFIC.
                 """
@@ -121,35 +112,39 @@ class NaiveBayesClassifier:
 
         return splits
 
-    """ Generates the features set for the training data """
+    """
+    Generates the features set for the training data.
+    """
     def _generateFeaturesSet(self):
         def generateDefaultFreq():
             frequencies = {}
-            for label in self.labels:
+            for label in self._labels:
                 frequencies[label] = 0
             return frequencies
 
-        featuresSet = {}
-        for text, labels in self._trainingSet.items():
-            tokens = re.split('[ .,-]', text) # split on space, period, comma, dash
+        featuresset = {}
+        for text, labels in self._trainingset.items():
+            # Split on these characters.
+            tokens = re.split('[ .,-]', text)
             for token in tokens:
-
-                if token not in self._commonWords:
-                    if token not in featuresSet:
-                        featuresSet[token] = generateDefaultFreq()
+                if token not in self._commonwords:
+                    if token not in featuresset:
+                        featuresset[token] = generateDefaultFreq()
                     # Loop through all labels associated with this feature.
                     for label in labels:
-                        featuresSet[token][label] += 1
-        self._featuresSet = featuresSet
+                        featuresset[token][label] += 1
+        self._featuresset = featuresset
 
-    """ Generates expected likelihood distribution for features. """
+    """
+    Generates expected likelihood distribution for features.
+    """
     def _generateFeatureProbabilityDistribution(self):
         frequencyDistributions = defaultdict(FreqDist)
         values = defaultdict(set)
-        numberSamples = len(self._trainingSet) /2 
+        numberSamples = len(self._trainingset) /2 
 
-        for token, counts in self._featuresSet.items():
-            for label in self.labels:
+        for token, counts in self._featuresset.items():
+            for label in self._labels:
                 frequencyDistributions[label, token].inc(True, count = counts[label])
                 frequencyDistributions[label, token].inc(None, numberSamples - counts[label])
                 values[token].add(None)
@@ -160,18 +155,7 @@ class NaiveBayesClassifier:
             eleProbDist = ELEProbDist(freqDist, bins=len(values[name]))
             probabilityDistribution[label, name] = eleProbDist
 
-        self._featureProbabilityDistribution = probabilityDistribution
-
-    """
-    Train the classifier.
-    @param trainingDirectory A valid absolute path from the training directory.
-    """
-    def train(self, trainingDirectory):
-        self._trainingDirectory = trainingDirectory
-        self._generateTrainingSet()
-        self._generateFeaturesSet()
-        self._generateFeatureProbabilityDistribution()
-
+        self._featureprobabilitydistribution = probabilityDistribution
 
     """
     Classifies an item.
@@ -197,8 +181,8 @@ class NaiveBayesClassifier:
     def _prob_classify(self, featureset):
         featureset = featureset.copy() 
         for fname in featureset.keys(): 
-            for label in self.labels: 
-                if (label, fname) in self._featureProbabilityDistribution: 
+            for label in self._labels: 
+                if (label, fname) in self._featureprobabilitydistribution: 
                     break 
             else: 
                 #print 'Ignoring unseen feature %s' % fname 
@@ -206,24 +190,25 @@ class NaiveBayesClassifier:
 
         # Start with a log probability of 0 to avoid skewing towards larger data sets
         logprob = {} 
-        for label in self.labels: 
+        for label in self._labels: 
             logprob[label] = 0                 
 
         # Add in the log probability of features given labels. 
 
-        for label in self.labels: 
+        for label in self._labels: 
             for (fname, fval) in featureset.items(): 
 
-                if (label, fname) in self._featureProbabilityDistribution: 
-                    feature_probs = self._featureProbabilityDistribution[label,fname] 
+                if (label, fname) in self._featureprobabilitydistribution: 
+                    feature_probs = self._featureprobabilitydistribution[label,fname] 
                     #print "log prob for " + str(label) +  " is " + str(feature_probs.logprob(fval) )
                     logprob[label] += feature_probs.logprob(fval) 
                 else: 
-                # nb: This case will never come up if the classifier was created by NaiveBayesClassifier.train(). 
+                # nb: This case will never come up if the classifier was created by 
+                # NaiveBayesClassifier.train(). 
                     logprob[label] += sum_logs([]) # = -INF.
         
-        ## print out the log prob for each label before normalizing
-        #for key,value in  self._featureProbabilityDistribution.items():
+        # print out the log prob for each label before normalizing
+        #for key,value in  self._featureprobabilitydistribution.items():
         #    print "key value of featureProbabilityDistribution " + str(key) + "," + str(value.freqdist() )
 
         #print "log prob with features is " + str(logprob)    
